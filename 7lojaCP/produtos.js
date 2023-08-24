5d6135588564c09d583cf2dd0116cf8489b099ea
vxfontes <nessa1vane@icloud.com>
2023-08-24 12:04:10 -0300

    feat: utilização de várias categorias

--- 7lojaCP/produtos.js
+++ 7lojaCP/produtos.js
@@ -1,15 +1,16 @@
-const categorias = ["Decoração", "Utilidades Domésticas", "Brinquedos", "Papelaria", "Cuidados Pessoais", "Eletrônicos", "Acessórios", "Roupas", "Calçados", "Alimentos", "Bebidas", "Jardinagem", "Ferramentas", "Artesanato", "Pet Shop", "Esportes", "Beleza", "Saúde", "Livros", "Festas e Eventos"]
+const categorias = ["Decoração", "Utilidades", "Brinquedos", "Papelaria", "Cuidados Pessoais", "Eletrônicos", "Acessórios", "Roupas", "Calçados", "Alimentos", "Bebidas", "Jardinagem", "Ferramentas", "Artesanato", "Pet Shop", "Esportes", "Beleza", "Saúde", "Livros", "Festas e Eventos"]
 
 const select = document.getElementById('categoria');
+const adicionando = document.getElementById('adicionando');
 const selectFiltro = document.getElementById('filtroCategoria');
 const cadastro = document.getElementById('cadastro');
-const filtro = document.getElementById('btnFiltro');
 const formProduto = document.getElementById('formProduto');
 const mensagens = document.getElementById('mensagens');
 const mensagemErroSucess = document.createElement('h5');
 const produtosContainer = document.getElementById('produtosContainer');
 
 const produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+const categoriasEscolhidas = []
 
 function renderizarCategorias(theID) {
     categorias.forEach(categoria => {
@@ -21,9 +22,9 @@ function renderizarCategorias(theID) {
 }
 
 function salvandoProduto(event) {
-    const { nome, valor, categoria } = event.target;
+    const { nome, valor } = event.target;
 
-    if (validarCampos(nome.value, valor.value, categoria.value)) {
+    if (validarCampos(nome.value, valor.value)) {
         mensagemErroSucess.textContent = `Produto ${nome.value} cadastrado com sucesso!`;
         mensagemErroSucess.style.color = '#634d91';
         mensagens.appendChild(mensagemErroSucess);
@@ -31,28 +32,25 @@ function salvandoProduto(event) {
         const produto = {
             nome: nome.value,
             valor: valor.value,
-            categoria: categoria.value,
+            categoria: categoriasEscolhidas,
         }
         produtos.push(produto);
         localStorage.setItem('produtos', JSON.stringify(produtos));
         formProduto.reset();
     } else {
-        mensagemErroSucess.textContent = 'Por favor, preencha todos os campos antes de enviar.';
+        mensagemErroSucess.textContent = 'Por favor, preencha todos os campos e adicione categorias antes de enviar.';
         mensagemErroSucess.style.color = 'red';
         mensagens.appendChild(mensagemErroSucess);
     }
 }
 
-function validarCampos(nome, valor, categoria) {
-    if (nome.trim() === '' || valor === '' || categoria === '') {
-        return false;
-    }
+function validarCampos(nome, valor) {
+    if (nome.trim() === '' || valor === '' || categoriasEscolhidas.length === 0) return false;
     return true;
 }
 
 function exibindoProduto(produto) {
     let valorPrimeiroDigito = parseInt(produto.valor.toString()[0]);
-
     let imagemSrc = "";
     if (valorPrimeiroDigito % 2 === 0) imagemSrc = "./assets/produto1.png";
     else imagemSrc = "./assets/produto2.png";
@@ -62,9 +60,7 @@ function exibindoProduto(produto) {
     cardDiv.innerHTML = `
         <div class="card" style="width: 100%; box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.2);">
             <div class="row no-gutters">
-                <div class="col-md-4">
-                    <img src="${imagemSrc}" class="card-img rounded-circle m-4" alt="foto do produto">
-                </div>
+                <div class="col-md-4"><img src="${imagemSrc}" class="card-img rounded-circle m-4" alt="foto do produto"></div>
                 <div class="col-md-1"></div>
                 <div class="col-md-7 mt-1">
                     <div class="card-body">
@@ -74,34 +70,44 @@ function exibindoProduto(produto) {
                         </div>
                         <hr>
                         <div class="align-self-end">
-                            <div style="display: flex">
-                                <p class="chip">${produto.categoria}</p>
+                            <div style="display: block">
+                                ${produto.categoria.map(cat => `<div style="display: flex; padding-bottom:3px"><p class="chip">${cat}</p></div>`).join('')}
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        `;
+    `;
+
     produtosContainer.appendChild(cardDiv);
+
 }
 
 function renderizarProdutos(filtroCategoria) {
     produtosContainer.innerHTML = '';
 
     produtos.forEach(produto => {
-        if (filtroCategoria === 'all') {
-            exibindoProduto(produto)
-        } else if (filtroCategoria === '' || produto.categoria === filtroCategoria) {
-            exibindoProduto(produto)
+        if (filtroCategoria === 'all' || produto.categoria.includes(filtroCategoria)) {
+            exibindoProduto(produto);
         }
     });
 }
 
+function adicionarCategorias() {
+    let categoriaSelect = document.getElementById("categoria").value;
+    categoriasEscolhidas.push(categoriaSelect)
+    mensagemErroSucess.textContent = `Categoria ${categoriaSelect} adicionada com sucesso!`;
+    mensagemErroSucess.style.color = '#634d91';
+    mensagens.appendChild(mensagemErroSucess);
+}
+
 cadastro.addEventListener('click', (() => renderizarCategorias()));
+adicionando.addEventListener('click', (() => adicionarCategorias()));
 formProduto.addEventListener('submit', ((event) => {
     event.preventDefault();
     salvandoProduto(event);
+    renderizarProdutos(`all`);
 }));
 document.addEventListener('DOMContentLoaded', () => {
     renderizarCategorias(select);
