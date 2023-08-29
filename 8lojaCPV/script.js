e95fbe742a3100aa86ef1dcd883a78d9815c19c0
vxfontes <nessa1vane@icloud.com>
2023-08-29 13:42:47 -0300

    feat: adicionando botão para todos

--- 8lojaCPV/script.js
+++ 8lojaCPV/script.js
@@ -1,13 +1,18 @@
 const getFile = document.getElementById('getFile');
 const vendedoresArray = JSON.parse(localStorage.getItem("vendedores")) || [];
+const produtosArray = JSON.parse(localStorage.getItem("produtos")) || [];
+const clientesArray = JSON.parse(localStorage.getItem("clientes")) || [];
 
 function getType() {
     const valor = getFile.value
 
     if(valor == 'vendedores') {
         enviandoValores({ vetor: vendedoresArray, type: valor })
+    } else if(valor == 'produtos') {
+        enviandoValores({ vetor: produtosArray, type: valor })
+    } else if(valor == 'clientes') {
+        enviandoValores({ vetor: clientesArray, type: valor })
     }
-
 }
 
 function enviandoValores(value) {
